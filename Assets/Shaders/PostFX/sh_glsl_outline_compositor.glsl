13eaf1fe71e162d67039a68c49c39fcbfa9b731b
Julian Kehr <juliankehr01@gmail.com>
2025-10-16 17:46:52 +0200

    c

--- Assets/Shaders/PostFX/sh_glsl_outline_compositor.glsl
+++ Assets/Shaders/PostFX/sh_glsl_outline_compositor.glsl
@@ -117,7 +117,9 @@ void main() {
 
 	//original *= vec4(getNormal(UV).rgb * 2.0 - 1.0, 1.0);
 	original = imageLoad(color_image, uv_pixel);
-	original = vec4(sampleLinearDepth(snapped_uv) / 100.0, 0.0, 0.0, 0.0);
+	float packed = uintBitsToFloat(packHalf2x16(original.xy));
+	// vec2 unpacked = unpackHalf2x16(floatBitsToUint(packed));
+	original = vec4(packed, original.y, original.z ,sampleLinearDepth(snapped_uv) / 100.0);
 	// original = vec4(UV.x, UV.y, 0.0, 1.0);
 
 	imageStore(color_image, uv_pixel, original);
